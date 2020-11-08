833f24cdb6ef0ed89215affea2e574fceace3b42
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2020-11-08 16:36:13 +0000

    Use variable

--- app/src/androidTest/java/com/crypto/currencyapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/crypto/currencyapp/ExampleInstrumentedTest.kt
@@ -1,4 +1,7 @@
 package com.crypto.currencyapp
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.Andr
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.juni
