0c1dd3b8bb009aa3393c577d683b78b4f98c41b4
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2020-11-16 10:24:21 +0000

    Create data.json

--- app/src/androidTest/java/com/crypto/currencyapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/crypto/currencyapp/ExampleInstrumentedTest.kt
@@ -18,4 +18,5 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.ge
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assert
