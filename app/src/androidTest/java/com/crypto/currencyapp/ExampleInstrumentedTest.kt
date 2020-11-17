d2f8bea6ba3c54224283893ada2ecf84a549837d
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2020-11-17 08:02:14 +0000

    chore: Update dependencies

--- app/src/androidTest/java/com/crypto/currencyapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/crypto/currencyapp/ExampleInstrumentedTest.kt
@@ -19,4 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.crypto.currencyapp
+        assertEquals("com.crypto.currencyapp", appContext.packageName)
+    }
+}
