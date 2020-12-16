d5ad6a39c5e50d4e24abae513a00e694db4b3a87
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2020-12-16 12:42:37 +0000

    initramfs: read CONFIG_RD_ variables for initramfs compression

--- app/src/main/java/com/crypto/currencyapp/CoinApp.kt
+++ app/src/main/java/com/crypto/currencyapp/CoinApp.kt
@@ -1,4 +1,7 @@
 package com.crypto.currencyapp
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidAp
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoinApp : Appl
