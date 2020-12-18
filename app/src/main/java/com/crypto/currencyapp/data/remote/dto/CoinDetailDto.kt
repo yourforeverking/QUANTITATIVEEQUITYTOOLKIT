5b6930ecb057ebbc64aa4347565150e6fc30ad77
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2020-12-18 12:11:57 +0000

    Release 1.4.3

--- app/src/main/java/com/crypto/currencyapp/data/remote/dto/CoinDetailDto.kt
+++ app/src/main/java/com/crypto/currencyapp/data/remote/dto/CoinDetailDto.kt
@@ -2,3 +2,33 @@
 package com.crypto.currencyapp.data.remote.dto
 
 import com.crypto.currencyapp.domain.model.CoinDetail
+import com.google.gson.annotations.SerializedName
+
+data class CoinDetailDto(
+    val contract: String,
+    val contracts: List<Contract>,
+    val description: String,
+    val development_status: String,
+    val first_data_at: String,
+    val hardware_wallet: Boolean,
+    val hash_algorithm: String,
+    val id: String,
+    @SerializedName("is_active")
+    val isActive: Boolean,
+    val is_new: Boolean,
+    val last_data_at: String,
+    val links: Links,
+    val links_extended: List<LinksExtended>,
+    val message: String,
+    val name: String,
+    val open_source: Boolean,
+    val org_structure: String,
+    val parent: Parent,
+    val platform: String,
+    val proof_type: String,
+    val rank: Int,
+    val started_at: String,
+    val symbol: String,
+    val tags: List<Tag>,
+    val team: List<TeamMember>,
+    val type: String,
