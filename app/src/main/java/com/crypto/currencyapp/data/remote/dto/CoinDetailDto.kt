f9840c77f354c23ae29efdd6299aa00845c9b605
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2020-12-19 15:22:12 +0000

    basic readme

--- app/src/main/java/com/crypto/currencyapp/data/remote/dto/CoinDetailDto.kt
+++ app/src/main/java/com/crypto/currencyapp/data/remote/dto/CoinDetailDto.kt
@@ -32,3 +32,18 @@ data class CoinDetailDto(
     val tags: List<Tag>,
     val team: List<TeamMember>,
     val type: String,
+    val whitepaper: Whitepaper
+)
+
+fun CoinDetailDto.toCoinDetail() : CoinDetail{
+    return CoinDetail(
+        coinId = id,
+        name = name,
+        description = description,
+        symbol = symbol,
+        rank = rank,
+        isActive = isActive,
+        tags = tags.map { it.name },
+        team = team
+    )
+}
