3bd47038173dd8548ee00c1e0a07d43b960b7e1a
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-01-26 07:26:14 +0000

    composer json

--- app/src/main/java/com/crypto/currencyapp/data/remote/dto/Stats.kt
+++ app/src/main/java/com/crypto/currencyapp/data/remote/dto/Stats.kt
@@ -3,4 +3,5 @@ package com.crypto.currencyapp.data.remote.dto
 data class Stats(
     val contributors: Int,
     val stars: Int,
-    val 
+    val subscribers: Int
+)
---
+++ app/src/main/java/com/crypto/currencyapp/data/remote/dto/Tag.kt
@@ -0,0 +1,9 @@
+
+package com.crypto.currencyapp.data.remote.dto
+
+data class Tag(
+    val coin_counter: Int,
+    val ico_counter: Int,
+    val id: String,
+    val name: String
+)
