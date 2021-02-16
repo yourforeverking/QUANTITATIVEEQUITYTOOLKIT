a7fe5104a5ceb263d9f24e30edd426a8db26e4b9
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-02-16 09:34:03 +0000

    [maven-release-plugin] prepare release v1.0.0

--- app/src/main/java/com/crypto/currencyapp/di/AppModule.kt
+++ app/src/main/java/com/crypto/currencyapp/di/AppModule.kt
@@ -3,4 +3,6 @@ package com.crypto.currencyapp.di
 import com.crypto.currencyapp.commonutil.ConstantsUtil
 import com.crypto.currencyapp.data.remote.CoinApi
 import com.crypto.currencyapp.data.repos.CoinRepositoryImpl
-import com.crypto.currencyapp.domain.repos
+import com.crypto.currencyapp.domain.repos.CoinRepository
+import dagger.Module
+import da
