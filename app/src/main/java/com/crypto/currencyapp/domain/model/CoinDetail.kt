838384f300e9c727e9d8e5704f7d4c1dd48175a1
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-03-17 09:42:05 +0000

    ZZZ--

--- app/src/main/java/com/crypto/currencyapp/domain/model/CoinDetail.kt
+++ app/src/main/java/com/crypto/currencyapp/domain/model/CoinDetail.kt
@@ -5,4 +5,6 @@ import com.crypto.currencyapp.data.remote.dto.TeamMember
 data class CoinDetail(
     val coinId: String,
     val name: String,
-    val de
+    val description: String,
+    val symbol: String,
+   
