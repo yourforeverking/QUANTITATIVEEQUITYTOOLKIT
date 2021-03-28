f9de34cf26b4f0a708796abbfd0dd84d65e0cc22
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-03-28 18:29:06 +0000

    Validation fix

--- app/src/main/java/com/crypto/currencyapp/domain/usecase/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/crypto/currencyapp/domain/usecase/get_coins/GetCoinsUseCase.kt
@@ -2,4 +2,4 @@ package com.crypto.currencyapp.domain.usecase.get_coins
 
 import com.crypto.currencyapp.commonutil.Resource
 import com.crypto.currencyapp.data.remote.dto.toCoin
-import com
+import com.crypto.currencyapp.domain.mo
