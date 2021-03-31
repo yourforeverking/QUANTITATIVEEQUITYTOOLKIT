be8fd1153e7fe1c79cf3bfd856c4e64b7ac1e97e
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-03-31 20:53:33 +0000

    code readability

--- app/src/main/java/com/crypto/currencyapp/domain/usecase/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/crypto/currencyapp/domain/usecase/get_coins/GetCoinsUseCase.kt
@@ -2,4 +2,5 @@ package com.crypto.currencyapp.domain.usecase.get_coins
 
 import com.crypto.currencyapp.commonutil.Resource
 import com.crypto.currencyapp.data.remote.dto.toCoin
-import com.crypto.currencyapp.domain.mo
+import com.crypto.currencyapp.domain.model.Coin
+import com.crypto.currency
