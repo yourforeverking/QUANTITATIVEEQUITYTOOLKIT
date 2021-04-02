8b11a17d7822576348231c0b207d4956a2df5a5e
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-04-02 12:25:16 +0000

    Avoid tox_install.sh for constraints support

--- app/src/main/java/com/crypto/currencyapp/domain/usecase/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/crypto/currencyapp/domain/usecase/get_coins/GetCoinsUseCase.kt
@@ -3,4 +3,5 @@ package com.crypto.currencyapp.domain.usecase.get_coins
 import com.crypto.currencyapp.commonutil.Resource
 import com.crypto.currencyapp.data.remote.dto.toCoin
 import com.crypto.currencyapp.domain.model.Coin
-import com.crypto.currency
+import com.crypto.currencyapp.domain.repos.CoinRepository
+import kot
