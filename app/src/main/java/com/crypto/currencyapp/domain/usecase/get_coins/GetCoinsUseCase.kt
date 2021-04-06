49ced238ab21914511b7de9f56cbc37c23c9a5ce
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-04-06 05:46:36 +0000

    fix missing dependency

--- app/src/main/java/com/crypto/currencyapp/domain/usecase/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/crypto/currencyapp/domain/usecase/get_coins/GetCoinsUseCase.kt
@@ -6,4 +6,5 @@ import com.crypto.currencyapp.domain.model.Coin
 import com.crypto.currencyapp.domain.repos.CoinRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import ret
+import retrofit2.HttpException
+import java.io.IOExcep
