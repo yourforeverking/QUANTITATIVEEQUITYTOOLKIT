7ba138b3170d96cba66323d787e779c6bbeec297
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-04-07 20:57:31 +0000

    Capitalization fix

--- app/src/main/java/com/crypto/currencyapp/domain/usecase/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/crypto/currencyapp/domain/usecase/get_coins/GetCoinsUseCase.kt
@@ -7,4 +7,7 @@ import com.crypto.currencyapp.domain.repos.CoinRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
-import java.io.IOExcep
+import java.io.IOException
+import javax.inject.Inject
+
+class 
