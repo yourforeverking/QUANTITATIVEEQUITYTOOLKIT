47477360a814587cc2165f08de3df22a55b5af5f
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-04-09 22:46:04 +0000

    f2fs: introduce f2fs_has_xattr_block for better readability

--- app/src/main/java/com/crypto/currencyapp/domain/usecase/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/crypto/currencyapp/domain/usecase/get_coins/GetCoinsUseCase.kt
@@ -10,4 +10,5 @@ import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
 
-class GetCoinsUseCase @Inject construct
+class GetCoinsUseCase @Inject constructor(
+    private val repository: CoinReposi
