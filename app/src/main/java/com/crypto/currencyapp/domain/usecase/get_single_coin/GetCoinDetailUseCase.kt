6c9e35c77b83c091f1a7297b292e6352e4284618
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-04-28 17:08:19 +0000

    cifs: fix parsing of password mount option

--- app/src/main/java/com/crypto/currencyapp/domain/usecase/get_single_coin/GetCoinDetailUseCase.kt
+++ app/src/main/java/com/crypto/currencyapp/domain/usecase/get_single_coin/GetCoinDetailUseCase.kt
@@ -1,4 +1,4 @@
 package com.crypto.currencyapp.domain.usecase.get_single_coin
 
 import com.crypto.currencyapp.commonutil.Resource
-import com.crypto.cu
+import com.crypto.currencyapp.data.remote.dto.toCoinDe
