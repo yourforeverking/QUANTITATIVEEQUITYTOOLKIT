14f8e8069a74a59f97771b4bbbfb7a11139c5ee8
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-05-02 10:40:54 +0000

    removed bower

--- app/src/main/java/com/crypto/currencyapp/domain/usecase/get_single_coin/GetCoinDetailUseCase.kt
+++ app/src/main/java/com/crypto/currencyapp/domain/usecase/get_single_coin/GetCoinDetailUseCase.kt
@@ -3,4 +3,5 @@ package com.crypto.currencyapp.domain.usecase.get_single_coin
 import com.crypto.currencyapp.commonutil.Resource
 import com.crypto.currencyapp.data.remote.dto.toCoinDetail
 import com.crypto.currencyapp.domain.model.CoinDetail
-import com.crypto.currencyapp.domain.repos.CoinReposi
+import com.crypto.currencyapp.domain.repos.CoinRepository
+import kotlinx.coroutines.f
