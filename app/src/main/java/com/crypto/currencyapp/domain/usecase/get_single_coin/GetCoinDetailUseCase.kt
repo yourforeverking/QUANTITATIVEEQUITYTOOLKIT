dd631ce89427cfe268a803e5b3d168f864f98052
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-05-02 21:37:33 +0000

    Update tfta_module.py

--- app/src/main/java/com/crypto/currencyapp/domain/usecase/get_single_coin/GetCoinDetailUseCase.kt
+++ app/src/main/java/com/crypto/currencyapp/domain/usecase/get_single_coin/GetCoinDetailUseCase.kt
@@ -4,4 +4,5 @@ import com.crypto.currencyapp.commonutil.Resource
 import com.crypto.currencyapp.data.remote.dto.toCoinDetail
 import com.crypto.currencyapp.domain.model.CoinDetail
 import com.crypto.currencyapp.domain.repos.CoinRepository
-import kotlinx.coroutines.f
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flo
