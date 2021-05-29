add340729210fe346d59c6a9a3774453233bb031
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-05-29 20:37:35 +0000

    adding console

--- app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
@@ -4,4 +4,5 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
-import androidx.compose.mat
+import androidx.compose.material.Surface
+import androidx.naviga
