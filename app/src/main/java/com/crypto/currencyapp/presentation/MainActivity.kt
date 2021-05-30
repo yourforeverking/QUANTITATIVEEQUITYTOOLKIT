a10383e1205199f53015dcbddaca29b9c076fbcc
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-05-30 15:43:52 +0000

    update: theme

--- app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
@@ -6,4 +6,5 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.navigation.compose.NavHost
-import androidx.navi
+import androidx.navigation.compose.composable
+import androidx.naviga
