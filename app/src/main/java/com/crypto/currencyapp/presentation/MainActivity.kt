5b068984094d0ee0e0d3a28297a62aa96d605f7c
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-05-30 10:47:43 +0000

    [ci skip] - no new overlapping errors

--- app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
@@ -5,4 +5,5 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.naviga
+import androidx.navigation.compose.NavHost
+import androidx.navi
