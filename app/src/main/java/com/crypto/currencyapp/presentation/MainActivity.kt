05a69fbb74ed696e09ce80d5563e77ca867cc5e7
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-06-03 20:05:30 +0000

    Update gem dependencies.

--- app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
@@ -8,4 +8,5 @@ import androidx.compose.material.Surface
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.crypto.currencyapp.presentatio
+import com.crypto.currencyapp.presentation.cryptodetail.CoinDetailScreen
+import 
