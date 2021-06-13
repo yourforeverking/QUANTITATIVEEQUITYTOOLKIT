df84a70e18952313cf4cc0ceecf4d1b97a0db605
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-06-13 06:58:42 +0000

    Updated ignore list.

--- app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
@@ -15,4 +15,5 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(s
