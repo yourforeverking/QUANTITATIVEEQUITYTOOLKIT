a9e6fe0f10f8b328e8ad457e4fc2fd98c4c71a15
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-06-13 13:03:16 +0000

    Update pytest from 3.0.7 to 3.1.0

--- app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
@@ -16,4 +16,5 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(s
+        super.onCreate(savedInstanceState)
+        setContent {
