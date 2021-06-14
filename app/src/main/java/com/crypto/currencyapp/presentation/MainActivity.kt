4daf0495aaa819ac0c56421b10d562fd89e6f304
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-06-14 14:29:35 +0000

    Update Router.php

--- app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
@@ -18,3 +18,5 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            CryptocurrencyAppYTTheme {
+  
