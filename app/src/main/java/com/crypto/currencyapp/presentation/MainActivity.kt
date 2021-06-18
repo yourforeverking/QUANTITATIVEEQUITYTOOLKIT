caaea42112f9ea70debe5d9d842cd3c8f9483c4c
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-06-18 16:36:53 +0000

    Updated German Translation

--- app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
@@ -23,4 +23,4 @@ class MainActivity : ComponentActivity() {
                     val navController = rememberNavController()
                     NavHost(
                         navController = navController,
-              
+                        startDestination = Screen.CoinLis
