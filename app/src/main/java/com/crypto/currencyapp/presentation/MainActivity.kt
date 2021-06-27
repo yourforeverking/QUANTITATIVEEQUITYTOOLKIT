c556c666dbbf6b04a032d7623cb07896f8cd2201
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-06-27 21:27:04 +0000

    OAK-527: permissions (wip)

--- app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
@@ -29,4 +29,5 @@ class MainActivity : ComponentActivity() {
                             route = Screen.CoinListScreen.route
                         ) {
                             CoinListScreen(navController)
-     
+                        }
+                    
