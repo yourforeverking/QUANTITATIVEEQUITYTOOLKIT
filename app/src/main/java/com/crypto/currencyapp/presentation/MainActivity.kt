21a8c6a025e831ef027cfcec60c4697d169192ee
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-06-29 13:13:21 +0000

    Update _config.yml

--- app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/MainActivity.kt
@@ -30,4 +30,5 @@ class MainActivity : ComponentActivity() {
                         ) {
                             CoinListScreen(navController)
                         }
+                        composable(
                     
