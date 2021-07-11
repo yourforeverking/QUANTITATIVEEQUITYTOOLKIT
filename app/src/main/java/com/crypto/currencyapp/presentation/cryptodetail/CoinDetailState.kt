f24ca9e8fd51783a39bcccd01172900da9b28f54
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-07-11 11:29:24 +0000

    added install target

--- app/src/main/java/com/crypto/currencyapp/presentation/cryptodetail/CoinDetailState.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/cryptodetail/CoinDetailState.kt
@@ -1,4 +1,6 @@
 package com.crypto.currencyapp.presentation.cryptodetail
 
 import com.crypto.currencyapp.domain.model.Coin
-import com.crypto
+import com.crypto.currencyapp.domain.model.CoinDetail
+
+data 
