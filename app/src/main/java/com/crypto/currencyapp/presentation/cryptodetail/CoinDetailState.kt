4dded572e115de806f5a1bf7fbaf52b5ac653a93
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-07-12 08:35:59 +0000

    Removing unused functions

--- app/src/main/java/com/crypto/currencyapp/presentation/cryptodetail/CoinDetailState.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/cryptodetail/CoinDetailState.kt
@@ -3,4 +3,5 @@ package com.crypto.currencyapp.presentation.cryptodetail
 import com.crypto.currencyapp.domain.model.Coin
 import com.crypto.currencyapp.domain.model.CoinDetail
 
-data 
+data class CoinDetailState(
+    val isL
