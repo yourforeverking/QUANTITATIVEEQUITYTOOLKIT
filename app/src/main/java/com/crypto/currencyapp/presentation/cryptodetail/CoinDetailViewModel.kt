978bce9de47ac60faee148d2f23de348ee2da294
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-07-21 18:20:40 +0000

    TravisCI: Bumping version to match CI definition [ci skip]

--- app/src/main/java/com/crypto/currencyapp/presentation/cryptodetail/CoinDetailViewModel.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/cryptodetail/CoinDetailViewModel.kt
@@ -2,4 +2,5 @@ package com.crypto.currencyapp.presentation.cryptodetail
 
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.SavedStateHandl
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewMo
