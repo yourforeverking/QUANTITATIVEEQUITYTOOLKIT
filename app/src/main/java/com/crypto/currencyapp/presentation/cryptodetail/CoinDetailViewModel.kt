2724d2955d7428523655ca69711dca0a6a8adf63
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-07-23 21:02:32 +0000

    Doc changes.

--- app/src/main/java/com/crypto/currencyapp/presentation/cryptodetail/CoinDetailViewModel.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/cryptodetail/CoinDetailViewModel.kt
@@ -3,4 +3,5 @@ package com.crypto.currencyapp.presentation.cryptodetail
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewMo
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModel
