a00d256699ab2d46ef60e5a6e9e4fd3a983e156c
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-08-05 22:51:00 +0000

    Update Kotlin version

--- app/src/main/java/com/crypto/currencyapp/presentation/cryptodetail/CoinDetailViewModel.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/cryptodetail/CoinDetailViewModel.kt
@@ -13,4 +13,6 @@ import com.crypto.currencyapp.domain.usecase.get_single_coin.GetCoinDetailUseCas
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-i
+import javax.inject.Inject
+
+@HiltViewMo
