80472291113974649b4ceecad34cb0eff812d5e2
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-08-27 21:17:45 +0000

    Update list.md

---
+++ app/src/main/java/com/crypto/currencyapp/presentation/cryptodetail/compos/TeamListItem.kt
@@ -0,0 +1,14 @@
+
+package com.crypto.currencyapp.presentation.cryptodetail.compos
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.dp
+import com.crypto.currencyapp.data.remote.dto.TeamMember
