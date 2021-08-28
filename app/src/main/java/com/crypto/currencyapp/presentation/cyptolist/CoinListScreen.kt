7e517a771435d5f3fcbf66f89965f03299b72301
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-08-28 16:56:17 +0000

    Release 5.0.2

--- app/src/main/java/com/crypto/currencyapp/presentation/cyptolist/CoinListScreen.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/cyptolist/CoinListScreen.kt
@@ -13,3 +13,40 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.crypto.currencyapp.presentation.Screen
+import com.crypto.currencyapp.presentation.cyptolist.compos.CoinListItem
+
+@Composable
+fun CoinListScreen(
+    navController: NavController,
+    viewModel: CoinListViewModel = hiltViewModel()
+) {
+    val state = viewModel.state.value
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(state.coins) { coin ->
+                CoinListItem(coin = coin, onItemClick = {
+                    navController.navigate(Screen.CoinDetailScreen.route + "/${coin.id}")
+                })
+
+            }
+        }
+        if (state.error.isNotBlank()) {
+            Text(
+                text = state.error,
+                color = MaterialTheme.colors.error,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 20.dp)
+                    .align(Alignment.Center)
+            )
+        }
+        if (state.isLoading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    }
+}
