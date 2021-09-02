c2f87937474a0998f9b212b8eff8ea4a709c4ecb
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-09-02 06:42:59 +0000

    ready update

--- app/src/main/java/com/crypto/currencyapp/presentation/cyptolist/CoinListState.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/cyptolist/CoinListState.kt
@@ -5,4 +5,5 @@ import com.crypto.currencyapp.domain.model.Coin
 data class CoinListState(
     val isLoading: Boolean = false,
     val coins: List<Coin> = emptyList(),
-    val erro
+    val error: String = ""
+)
