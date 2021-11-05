001e835438b72b9b664eb972be479962f4cc0b0b
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-11-05 17:04:55 +0000

    Bump version to v0.3.0

--- app/src/main/java/com/crypto/currencyapp/presentation/cyptolist/compos/CoinListItem.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/cyptolist/compos/CoinListItem.kt
@@ -3,4 +3,5 @@ package com.crypto.currencyapp.presentation.cyptolist.compos
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWi
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.
