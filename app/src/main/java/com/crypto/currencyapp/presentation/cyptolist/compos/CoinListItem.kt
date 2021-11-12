aadeca3c1a38e3dc073384e846832c374c5b12c4
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-11-12 12:45:46 +0000

    update abstract

--- app/src/main/java/com/crypto/currencyapp/presentation/cyptolist/compos/CoinListItem.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/cyptolist/compos/CoinListItem.kt
@@ -10,4 +10,5 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
 import androidx.compose
