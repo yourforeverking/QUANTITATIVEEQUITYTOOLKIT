c4b45d97d81d97128bfc2fbcf36da4eaa04ee3c1
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-11-12 17:27:58 +0000

    final tweak

--- app/src/main/java/com/crypto/currencyapp/presentation/cyptolist/compos/CoinListItem.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/cyptolist/compos/CoinListItem.kt
@@ -11,4 +11,5 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose
+import androidx.compose.ui.graphics.Color
+import andr
