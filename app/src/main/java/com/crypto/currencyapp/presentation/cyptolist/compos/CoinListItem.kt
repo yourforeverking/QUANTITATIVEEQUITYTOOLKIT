2aa5494d1b231ed155e4b888e4a79153f390a1dc
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-11-14 15:50:24 +0000

    Update favicon.

--- app/src/main/java/com/crypto/currencyapp/presentation/cyptolist/compos/CoinListItem.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/cyptolist/compos/CoinListItem.kt
@@ -15,4 +15,5 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.
+import androidx.compose.ui.unit.dp
+impor
