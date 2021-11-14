d62eeb0dab47eb31b5d4459636b35c5be5a772c2
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-11-14 15:10:22 +0000

    Released v0.2.2

--- app/src/main/java/com/crypto/currencyapp/presentation/cyptolist/compos/CoinListItem.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/cyptolist/compos/CoinListItem.kt
@@ -14,4 +14,5 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tex
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.
