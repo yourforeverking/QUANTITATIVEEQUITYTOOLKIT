214bd7e7e18accfb4f323721b28fe2fd3bd3cd0e
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-11-22 10:41:26 +0000

    Remove leftover.

--- app/src/main/java/com/crypto/currencyapp/presentation/cyptolist/compos/CoinListItem.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/cyptolist/compos/CoinListItem.kt
@@ -31,4 +31,5 @@ fun CoinListItem(
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
-            text = "${coin.rank}. ${coin.name} ${coin.symbol
+            text = "${coin.rank}. ${coin.name} ${coin.symbol}",
+            style = MaterialTheme.ty
