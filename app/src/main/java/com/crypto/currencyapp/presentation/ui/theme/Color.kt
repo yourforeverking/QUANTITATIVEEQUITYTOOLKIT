df3a3d5fb900a53b88e9e9d4b4745c627052756b
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-12-04 06:46:46 +0000

    Remove useless comments

--- app/src/main/java/com/crypto/currencyapp/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/ui/theme/Color.kt
@@ -3,4 +3,5 @@ package com.crypto.currencyapp.presentation.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val DarkGray = Color(0xFF202020)
-val Med
+val MediumGray = Color(0xFF505050)
+val ColorPrimary = 
