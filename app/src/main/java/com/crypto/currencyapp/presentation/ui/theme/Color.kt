357bab751f33457e4bf3cf0f633beb6ea25356c6
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-12-05 15:11:10 +0000

    Update CONFIGURATION.md

--- app/src/main/java/com/crypto/currencyapp/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/ui/theme/Color.kt
@@ -5,4 +5,4 @@ import androidx.compose.ui.graphics.Color
 val DarkGray = Color(0xFF202020)
 val MediumGray = Color(0xFF505050)
 val ColorPrimary = Color(0xFF08FF04)
-val TextWhite = Color(0xFF
+val TextWhite = Color(0xFFEEEEEE)
