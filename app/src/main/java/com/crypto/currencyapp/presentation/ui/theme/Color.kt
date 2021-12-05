cf7de44df81e78add9692b94fdd432889b7daade
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-12-05 10:29:43 +0000

    v1.0.20

--- app/src/main/java/com/crypto/currencyapp/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/ui/theme/Color.kt
@@ -4,4 +4,5 @@ import androidx.compose.ui.graphics.Color
 
 val DarkGray = Color(0xFF202020)
 val MediumGray = Color(0xFF505050)
-val ColorPrimary = 
+val ColorPrimary = Color(0xFF08FF04)
+val TextWhite = Color(0xFF
