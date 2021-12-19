432b970eca0e59cbd9edc92675f677ada4b22225
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2021-12-19 11:09:55 +0000

    Pontoon: Update Asturian (ast) localization of Common Voice

--- app/src/main/java/com/crypto/currencyapp/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/crypto/currencyapp/presentation/ui/theme/Theme.kt
@@ -3,4 +3,5 @@ package com.crypto.currencyapp.presentation.ui.theme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
-import androidx.comp
+import androidx.compose.runtime.Composable
+import a
