3557a24e6320e27228e85d5ae84b5476cf527b6b
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2022-06-09 05:29:39 +0000

    Rename README.md to readme.md

---
+++ app/src/test/java/com/crypto/currencyapp/ExampleUnitTest.kt
@@ -0,0 +1,13 @@
+
+package com.crypto.currencyapp
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
