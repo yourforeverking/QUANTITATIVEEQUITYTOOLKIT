257d428753f80752ccb5784b3a3b432878f4c636
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2022-11-05 17:24:58 +0000

    Update RELEASING.md

--- gradlew.bat
+++ gradlew.bat
@@ -3,4 +3,6 @@
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
-@rem You may obtain a copy
+@rem You may obtain a copy of the License at
+@rem
+@rem      htt
