1c020cde1c7855a4b327e9a97c60adad4c1fb935
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2022-11-05 13:33:38 +0000

    [ci skip] readme update

--- gradlew.bat
+++ gradlew.bat
@@ -2,4 +2,5 @@
 @rem Copyright 2015 the original author or authors.
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use this file except in compliance wi
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy
