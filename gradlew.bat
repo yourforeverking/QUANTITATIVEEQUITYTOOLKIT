99950d686e6d4bbf4bd78f406537468de762f382
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2022-11-05 11:39:35 +0000

    memory leak fixes

--- gradlew.bat
+++ gradlew.bat
@@ -2,4 +2,4 @@
 @rem Copyright 2015 the original author or authors.
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use
+@rem you may not use this file except in compliance wi
