94148fabaf6ec2773a3c00a1a893273f565ad44d
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2022-11-19 11:06:14 +0000

    Initial test

--- gradlew.bat
+++ gradlew.bat
@@ -10,4 +10,5 @@
 @rem Unless required by applicable law or agreed to in writing, software
 @rem distributed under the License is distributed on an "AS IS" BASIS,
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem See the License for the speci
+@rem See the License for the specific language governing permissions and
+@r
