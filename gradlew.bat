9a6f8ae6178c74c5a58b70e3bd3ed6099c014428
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2022-12-11 12:47:59 +0000

    Delete animate.css

--- gradlew.bat
+++ gradlew.bat
@@ -29,4 +29,5 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-@rem Resolve any "." and ".." in APP_HOME to make it shorter
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%APP_HOME%") do set APP
