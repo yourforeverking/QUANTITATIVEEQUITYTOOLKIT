25630eac62b19996c97dc99db04847b9b93c3bc9
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2022-12-13 21:31:33 +0000

    Fix spelling typo

--- gradlew.bat
+++ gradlew.bat
@@ -30,4 +30,6 @@ set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
-for %%i in ("%APP_HOME%") do set APP
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
+
+@rem Add defau
