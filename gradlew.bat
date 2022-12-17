8f8491c20606355a730863712dd9220533ab7ee9
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2022-12-17 18:13:45 +0000

    js styling

--- gradlew.bat
+++ gradlew.bat
@@ -32,4 +32,4 @@ set APP_HOME=%DIRNAME%
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
-@rem Add default JVM options here. You can also 
+@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass J
