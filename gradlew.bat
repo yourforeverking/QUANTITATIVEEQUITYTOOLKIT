03acf3c0b6262dd82e23a1f51d17ae884641f764
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2022-12-18 20:14:36 +0000

    chore(package): update eslint to version 2.4.0

--- gradlew.bat
+++ gradlew.bat
@@ -32,4 +32,5 @@ set APP_HOME=%DIRNAME%
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass J
+@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+set DEFAULT_JVM_OP
