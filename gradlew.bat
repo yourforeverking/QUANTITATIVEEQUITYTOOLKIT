5350fd69598efc450022673ef4c12c0032e1599c
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2022-12-24 22:05:07 +0000

    Add archive page

--- gradlew.bat
+++ gradlew.bat
@@ -36,4 +36,7 @@ for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
 @rem Find java.exe
-if defined JAVA_HOME goto findJavaFromJavaH
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+set JAVA_EXE=java.exe
+%JAVA_EXE% -versi
