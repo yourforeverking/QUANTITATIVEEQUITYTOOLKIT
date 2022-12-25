2d9967919c365b0c7e3b670040dacdaadaf109ff
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2022-12-25 19:51:56 +0000

    fixing test cases

--- gradlew.bat
+++ gradlew.bat
@@ -45,4 +45,4 @@ if "%ERRORLEVEL%" == "0" goto execute
 echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
-echo Please 
+echo Please set the JAVA_HOME variable in your environm
