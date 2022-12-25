08881247e6e46823cfd7a0fd4e50926d3a8d00e9
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2022-12-25 09:23:38 +0000

    (Grav GitSync) Automatic Commit from mikegcox

--- gradlew.bat
+++ gradlew.bat
@@ -39,4 +39,5 @@ set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
-%JAVA_EXE% -versi
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto 
