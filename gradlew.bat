67f88a62fd815943f6d6db3fe9005999ca4e85ab
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2022-12-25 19:05:14 +0000

    Add Python3 yoga unit tests

--- gradlew.bat
+++ gradlew.bat
@@ -43,4 +43,6 @@ set JAVA_EXE=java.exe
 if "%ERRORLEVEL%" == "0" goto execute
 
 echo.
-echo ERROR: JAVA_HOME is not set and no 'java' command coul
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please 
