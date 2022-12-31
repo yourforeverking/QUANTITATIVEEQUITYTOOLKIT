0b62c1446dc60204c09bde6d5223f2050bfb5efe
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2022-12-31 19:01:50 +0000

    Adds License

--- gradlew.bat
+++ gradlew.bat
@@ -45,4 +45,5 @@ if "%ERRORLEVEL%" == "0" goto execute
 echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
-echo Please set the JAVA_HOME variable in your environm
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of
