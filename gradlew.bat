8a4f88b91f7f0697e0239857bea74ca0426fd0df
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2023-01-14 20:38:41 +0000

    fix constraints

--- gradlew.bat
+++ gradlew.bat
@@ -46,4 +46,6 @@ echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
-echo location of
+echo location of your Java installation.
+
+goto fail
