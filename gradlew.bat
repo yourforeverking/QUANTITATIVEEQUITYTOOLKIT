758f8c366e6ae8a4272b293109eb7c62eebf5f5f
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2023-02-05 05:46:33 +0000

    update specs

--- gradlew.bat
+++ gradlew.bat
@@ -54,4 +54,7 @@ goto fail
 set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
-if exist "%JAVA_EXE%" goto exe
+if exist "%JAVA_EXE%" goto execute
+
+echo.
+echo ERROR: JAVA_HOME is set to an inva
