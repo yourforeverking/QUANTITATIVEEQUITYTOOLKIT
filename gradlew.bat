405e6d6cff44d904a8234f76115c3f84236db9e8
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2023-02-05 06:04:35 +0000

    [ci skip] Update readme

--- gradlew.bat
+++ gradlew.bat
@@ -57,4 +57,6 @@ set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 if exist "%JAVA_EXE%" goto execute
 
 echo.
-echo ERROR: JAVA_HOME is set to an inva
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo
