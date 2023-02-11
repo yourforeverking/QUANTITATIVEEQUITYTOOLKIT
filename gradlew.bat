8784fd4e4e6fdf5c53c71736d29eccb5a8b9f954
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2023-02-11 07:41:41 +0000

    updated version in package.json

--- gradlew.bat
+++ gradlew.bat
@@ -60,4 +60,8 @@ echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java in
+echo location of your Java installation.
+
+goto fail
+
+:execute
