fef16e1657a06471a3405974ba9cb2ddf094029b
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2023-02-18 19:31:20 +0000

    Updated changelog.txt

--- gradlew.bat
+++ gradlew.bat
@@ -71,4 +71,4 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 
 @rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gr
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpa
