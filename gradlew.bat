4e1567d349364bbe375f493cdc9759d352f34e14
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2023-02-18 22:37:44 +0000

    md: flush writes before starting a recovery.

--- gradlew.bat
+++ gradlew.bat
@@ -71,4 +71,4 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 
 @rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpa
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.Gra
