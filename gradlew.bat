10381b923a7e064da6c3e41fc9dc6f5f3fa950e5
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2023-03-12 12:30:50 +0000

    Delete a.out

--- gradlew.bat
+++ gradlew.bat
@@ -78,4 +78,4 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail
-rem Set variable GRADLE_
+rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ retu
