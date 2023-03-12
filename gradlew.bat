17803abe5643dfda314835946614c933e4793581
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2023-03-12 18:23:03 +0000

    updated...

--- gradlew.bat
+++ gradlew.bat
@@ -78,4 +78,5 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail
-rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ retu
+rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+rem the _c
