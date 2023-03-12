c83f36646e23a9f206fb5bcbd0a8a72c7c3a5b83
yourforeverking <110723334+yourforeverking@users.noreply.github.com>
2023-03-12 18:50:02 +0000

    added express

--- gradlew.bat
+++ gradlew.bat
@@ -79,4 +79,5 @@ if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
-rem the _c
+rem the _cmd.exe /c_ return code!
+if  
